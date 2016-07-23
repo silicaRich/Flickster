348921ad99ca6bd20b9235c2fd6638fb6bdf5eef
Nicole Flokos <nicole.flokos@gmail.com>
2016-07-23 00:18:11 -0400

    SUCH JSON MUCH API :100:

--- app/src/main/java/com/codepath/flickster/MovieActivity.java
+++ app/src/main/java/com/codepath/flickster/MovieActivity.java
@@ -2,6 +2,11 @@ package com.codepath.flickster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.loopj.android.http.*;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response){
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+
     }
 }
